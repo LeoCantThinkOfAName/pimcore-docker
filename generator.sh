668700aa31f3ae390e817f85667c553db06850cb
LeoCantThinkofAName <i7851@hotmail.com>
2020-09-03 17:39:09 +0800

    edit bash script

--- generator.sh
+++ generator.sh
@@ -5,7 +5,7 @@ CYAN='\033[1;36m'
 YELLOW='\033[1;33m'
 RED='\033[1;31m'
 RESET='\033[0m'
-LOGO="${CYAN}                       ::::::::: :::::::::::   :::   :::    ::::::::   ::::::::  :::::::::  ::::::::::
+LOGO="\n\n${CYAN}                       ::::::::: :::::::::::   :::   :::    ::::::::   ::::::::  :::::::::  ::::::::::
                       :+:    :+:    :+:      :+:+: :+:+:  :+:    :+: :+:    :+: :+:    :+: :+:
                      +:+    +:+    +:+     +:+ +:+:+ +:+ +:+        +:+    +:+ +:+    +:+ +:+
                     +#++:++#+     +#+     +#+  +:+  +#+ +#+        +#+    +:+ +#++:++#:  +#++:++#
@@ -18,8 +18,7 @@ LOGO="${CYAN}                       ::::::::: :::::::::::   :::   :::    :::::::
            :#:        +#++:++#   +#+ +:+ +#+ +#++:++#   +#++:++#:  +#++:++#++: +#+    +#+    +:+ +#++:++#:
           +#+   +#+# +#+        +#+  +#+#+# +#+        +#+    +#+ +#+     +#+ +#+    +#+    +#+ +#+    +#+
          #+#    #+# #+#        #+#   #+#+# #+#        #+#    #+# #+#     #+# #+#    #+#    #+# #+#    #+#
-         ########  ########## ###    #### ########## ###    ### ###     ### ###     ########  ###    ###${RESET}\n
-                                        Your Path to be a sad F2E developer\n"
+         ########  ########## ###    #### ########## ###    ### ###     ### ###     ########  ###    ###${RESET}\n\n\n"
 
 printLogo() {
   # this function will print the annoy logo and keep the terminal clean (with the freakin logo of course)
@@ -27,40 +26,67 @@ printLogo() {
   printf "${LOGO}"
 }
 
-replaceMountpoint () {
+replaceStringFromFile() {
   # this function will replace the mountpoint placeholder to user's input
-  local search='$MOUNTPOINT;'
-  local replace=$MOUNTPOINT
+  local search=$1
+  local replace=$2
+  local file=$3
 
-  sed -i "s/${search}/${replace}/g" test.txt
+  sed -i "s/${search}/${replace}/g" $file
 }
 
 printLogo
 # prompt user for project name
 read -p $'\e[32mYour Project Name: \e[0m' PROJECT
+
+# check if folder exist
+while [ -d "pimcore-$PROJECT" ]; do
+    printLogo
+    printf "${RED}Project already exist! Please pick another name...\n"
+    read -p $'\e[32mYour Project Name: \e[0m' PROJECT
+done
+
 printLogo
 read -p $'\e[32mYour Docker Mount point: \e[0m' MOUNTPOINT
 
-replaceMountpoint
+printLogo
+# create project folder and checkout all files from docker branch
+printf "${GREEN}
+########################################################################################################################
+# PREPARE YOUR PROJECT DIRECTORY...                                                                                    #
+########################################################################################################################
+${RESET}\n"
+mkdir pimcore-$PROJECT
 
-# create project folder by fetching docker-compse file from github
-# echo "${GREEN}
-# ########################################################################################################################
-# # PREPARE YOUR PROJECT DIRECTORY...                                                                                    #
-# ########################################################################################################################
-# ${RESET}"
-# git clone https://github.com/LeoCantThinkOfAName/batch.git ./pimcore-$PROJECT
+git checkout docker -- pimcore-generator-docker/
 
-# # download docker image from dockerhub
-# echo "${GREEN}
-# ########################################################################################################################
-# # PREPARE DOCKER IMAGE...                                                                                              #
-# ########################################################################################################################
-# ${RESET}"
+# copy all files from branch folder to project folder, and delete it
+cp -R pimcore-generator-docker/* pimcore-$PROJECT/
+rm -rf pimcore-generator-docker
+# clear out git commit from docker branch
+git reset HEAD pimcore-generator-docker
 
-# install composer
-# install php7.0-gd
-# install php7.0-pdo_mysql
+printLogo
+# download docker image from dockerhub
+echo "${GREEN}
+########################################################################################################################
+# PREPARE DOCKER IMAGE...                                                                                              #
+########################################################################################################################
+${RESET}\n"
+
+docker pull leocantthinkofaname/lemon-pimcore:latest
+docker pull mysql:5.7
+
+# cd into project folder for the rest commands
+cd pimcore-$PROJECT
+
+# replace all {PROJECT} inside docker-compose file
+replaceStringFromFile "{PROJECT}" "$PROJECT" docker-compose.yml
+
+# replace mountpoint inside docker-compose file
+# but docker toolbox is just too stupid, giveup...
+replaceStringFromFile "{MOUNTPOINT}" "{!!!!!!CHANGE ME!!!!!!}" docker-compose.yml
+
 
 options[0]="Vim (Inside container)"
 options[1]="Bash (Inside container)"
@@ -69,16 +95,16 @@ options[2]="php My Admin (Extra container for phpmyadmin)"
 #Actions to take based on selection
 function ACTIONS {
     if [[ ${choices[0]} ]]; then
-        # run apk add vim in container
-        echo "Option 1 selected"
+        # uncommant vim to install
+        replaceStringFromFile "# {VIM}" "" dockerfiles/web/Dockerfile
     fi
     if [[ ${choices[1]} ]]; then
-        # run apk add bash in container
-        echo "Option 2 selected"
+        # uncommant bash to install
+        replaceStringFromFile "# {BASH}" "" dockerfiles/web/Dockerfile
     fi
-    if [[ !${choices[2]} ]]; then
-        # if pypmyadmin is not selected, remove php my admin from docker-compose.yml
-        echo "Option 3 selected"
+    if [[ ${choices[2]} ]]; then
+        # uncommant phpmyadmin to pull image
+        replaceStringFromFile "#" "" docker-compose.yml
     fi
 }
 
@@ -118,3 +144,13 @@ while MENU && read -e -p $'\e[32mUse number key to select:\e[0m' -n1 SELECTION &
 done
 
 ACTIONS
+
+printLogo
+
+printf "
+${GREEN}#########################################################################################################################
+## Finalize install                                                                                                     #
+#########################################################################################################################${RESET}\n\n"
+printf "${GREEN}Your Project is ready!${RESET}\n"
+printf "${GREEN}User${RESET}: admin\n"
+printf "${GREEN}Password${RESET}: Aa_123456789\n"
